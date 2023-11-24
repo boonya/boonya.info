4287d256263f095a05cc74fde7bc3ae46a46f5c5
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-11-24 23:48:23 +0100

    Pagination

--- src/components/Pagination/Item.tsx
+++ src/components/Pagination/Item.tsx
@@ -3,16 +3,17 @@ import Link from 'next/link';
 type Props = {
   index: boolean;
   number: number;
+  label: string | number;
   current: number | null;
 };
 
-export default function Item({index, number, current}: Props) {
+export default function Item({index, number, label, current}: Props) {
   const ariaCurrent = (index && !current) || current === number;
   const href = index ? `/` : `/page/${number}`;
   return (
     <li>
       <Link aria-current={ariaCurrent} href={href}>
-          {number}
+          {label}
       </Link>
     </li>
   );
--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -13,17 +13,39 @@ export default function Pagination({length}: Props) {
     .map((v) => Number(v) + 1)
     .toReversed();
 
+  const isFirst = !current || current === array.at(0);
+  const isLast = current === array.at(-1);
+  const prev = isFirst ? null : current && current - 1 || array.at(0);
+  const next = isLast ? null : current && current + 1 || array.at(1);
+
   return (
-    <nav aria-label="Page navigation" className="pagination">
-      <ul>
+    <nav aria-label="Page navigation" className="flex justify-center">
+      <ul className="flex justify-center gap-8">
+        {prev && (
+          <Item
+              index
+              current={current}
+              number={prev}
+              label={'<'}
+          />
+        )}
         {array.map((number, index) => (
           <Item
             key={number}
             index={index === 0}
             current={current}
             number={number}
+            label={number}
           />
         ))}
+        {next && (
+          <Item
+              index={false}
+              current={current}
+              number={next}
+              label={'>'}
+          />
+        )}
       </ul>
     </nav>
   );
