ba199e871919e068c6de51fe97ed34c53b42fe3a
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-12-08 11:30:58 +0100

    One more try

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps<Record<string, un
   useEffect(() => {
     const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID?.trim();
 
-    console.log('GOOGLE_ANALYTICS_ID:', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
+    console.log('GOOGLE_ANALYTICS_ID:', GOOGLE_ANALYTICS_ID);
     console.log('NODE_ENV:', process.env.NODE_ENV);
 
     if (GOOGLE_ANALYTICS_ID) {
