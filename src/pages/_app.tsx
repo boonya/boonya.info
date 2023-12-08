1acdc4510769c74f8a2b7968be8fc48df27a1e8a
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-12-08 10:29:13 +0100

    GA4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 // These styles apply to every route in the application
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 
 export default function App({ Component, pageProps }: AppProps<Record<string, unknown>>) {
+  useEffect(() => {
+    ReactGA.initialize('396647624');
+  }, []);
   return <Component {...pageProps} />;
 }
