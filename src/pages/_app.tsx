ecb7d351e3bbbe7de292bcb0653afafbee43668a
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-12-09 15:14:38 +0100

    Styles (#7)
    * Clean up
    
    * Use Shantell Sans font instead of Pangolin as the last one does not support different font-weight property.
    
    * Workflows

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,6 @@ import ReactGA from 'react-ga4';
 export default function App({ Component, pageProps }: AppProps<Record<string, unknown>>) {
   useEffect(() => {
     const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID?.trim();
-
-    console.log('GOOGLE_ANALYTICS_ID:', GOOGLE_ANALYTICS_ID);
-    console.log('NODE_ENV:', process.env.NODE_ENV);
-
     if (GOOGLE_ANALYTICS_ID) {
       ReactGA.initialize(GOOGLE_ANALYTICS_ID);
     }
