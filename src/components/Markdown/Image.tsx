fa7ae1883f8b388eb46ad776359da418b66d73e3
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-12-10 12:37:31 +0100

    Article about rooting a vacuum + code tag style

--- src/components/Markdown/Image.tsx
+++ src/components/Markdown/Image.tsx
@@ -1,17 +1,8 @@
 import NextImage, { ImageProps } from 'next/image';
 import deepmerge from 'deepmerge';
 
-export default function Image(props: ImageProps) {
+export default function Image({ src, alt, ...props }: ImageProps) {
   // @ts-expect-error Ok so far.
-  const src = props.src.replace(/^assets\//u, '/assets/');
-  // const newProps = deepmerge(
-  //   {
-  //     style: { objectFit: 'contain' },
-  //     width: 1200,
-  //     height: 800,
-  //   },
-  //   props,
-  // );
-  // return <NextImage {...newProps} src={src} />;
-  return <img {...props} src={src} />;
+  const _src = src.replace(/^assets\//u, '/assets/');
+  return <img {...props} alt={alt || 'image'} src={_src} />;
 }
