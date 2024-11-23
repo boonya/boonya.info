c157df406e217efceafb2f9f2deb4296d3487534
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2024-11-23 10:28:41 +0100

    Better styles

--- src/components/Markdown/Code.tsx
+++ src/components/Markdown/Code.tsx
@@ -28,18 +28,19 @@ export default function Code({className, children, ...rest}: Props) {
   }
 
   return (
-    <div className="relative">
+    <div className="group relative">
       <SyntaxHighlighter
         language={lang}
         showLineNumbers
         useInlineStyles={false}
-        className={clsx('mb-4 mt-4 overflow-auto rounded-md bg-slate-100 dark:bg-cyan-950', className)}
+        className={clsx('mb-4 mt-4 rounded-md bg-slate-100 dark:bg-cyan-950', className)}
       >
         {children}
       </SyntaxHighlighter>
       <button
-        className="btn absolute right-0 top-0 text-xs"
+        className="btn absolute right-0 top-0 text-xs opacity-0 transition-opacity duration-300 group-hover:opacity-100"
         aria-label="Copy source to the clipboard"
+        title="Copy source to the clipboard"
         onClick={() => navigator.clipboard.writeText(children)}
       >
         Copy
